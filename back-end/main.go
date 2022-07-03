c8797dd5d4f6c22abdf21a966baef2bce4475c39
Ethan <leesb647@gmail.com>
2022-07-03 21:51:31 +0900

    set up Gin framework

---
+++ back-end/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Gin Framework 설치 완료",
+		})
+	})
+	r.Run()
+}
